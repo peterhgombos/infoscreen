da8210b4f5ee3cce0bdc7cbad397520178be709c
Péter H. Gombos <peter@gombos.no>
2014-01-31 21:45:41 +0100

    Sets up example app.

---
+++ app/main.js
@@ -0,0 +1,28 @@
+var express = require('express')
+  , stylus = require('stylus')
+  , nib = require('nib')
+
+var routes = require('./routes')
+
+var app = express()
+
+function compile(str, path) {
+  return stylus(str)
+    .set('filename', path)
+    .use(nib())
+}
+
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jade')
+app.use(express.logger('dev'))
+app.use(stylus.middleware(
+  { src: __dirname + '/public'
+  , compile: compile
+  }
+))
+app.use(express.static(__dirname + '/public'))
+
+
+app.get('/', routes.index)
+
+app.listen(3000)
